31162e6ca7867fcdfb80311d99c024f426262947
teverist <tobyeverist1@gmail.com>
2019-12-09 17:34:49 +0000

    Added redux and reducers

---
+++ app/javascript/actions/session_actions.js
@@ -0,0 +1,36 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser,
+});
+
+const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors,
+});
+
+export const login = user => dispatch => (
+  SessionAPIUtil.login(user).then(loginUser => (
+    dispatch(receiveCurrentUser(loginUser))
+  ), err => (
+    dispatch(receiveSessionErrors(err.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch => (
+  SessionAPIUtil.logout().then(user => (
+    dispatch(receiveCurrentUser(null))
+  ))
+);
+
+export const signup = user => dispatch => (
+  SessionAPIUtil.signup(user).then(newUser => (
+    dispatch(receiveCurrentUser(newUser))
+  ), err => (
+      dispatch(receiveSessionErrors(err.responseJSON))
+  ))
+);
