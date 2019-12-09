96295514e996afbc0e33df7287d26886634ab81c
teverist <tobyeverist1@gmail.com>
2019-12-09 18:00:34 +0000

    Added login forms

--- app/javascript/trailgram.jsx
+++ app/javascript/trailgram.jsx
@@ -4,12 +4,18 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Index from './components/index';
 
+// test
+import * as actions from './actions/session_actions';
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
   
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  window.login = actions.login;
 
   const indexEl = document.getElementById("index");
 
